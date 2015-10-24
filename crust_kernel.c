6e8b29c95649d0b2f8359a12984b5f7922dddc6c
Jit <kanetkarster@gmail.com>
2015-10-24 12:47:38 -0400

    removed includes from crust_kernel

--- crust_kernel.c
+++ crust_kernel.c
@@ -1,8 +1,6 @@
 #include "include/crust_kernel.h"
 
 #include <mini-os/os.h>
-#include <mini-os/hypervisor.h>
-#include <mini-os/events.h> // init_events()
 #include <mini-os/lib.h> /* for printk, memcpy */
 
 void start_crust(void) {
